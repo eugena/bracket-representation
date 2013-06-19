4f41888ac97d6428008f4572ccb6e57a2e1a2796
Eugena A. Mikhailikova <eugena@inbox.ru>
2013-06-19 14:24:45 +0600

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='bracket_representation',
+    version='0.1',
+    description='Operations with bracket representations of trees',
+    author='Eugena Mihailikova',
+    author_email='eugena@inbox.ru',
+    url='https://github.com/eugena/bracket_representation',
+    packages=['bracket_representation', ], )
